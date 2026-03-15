4d2a270098ad0667a3ddd6f1018199a57a9b69b6
Angel Guzman <angelteo0510@gmail.com>
2026-03-14 19:06:52 -0600

    TanStack Query-Gestor de estado async

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -2,10 +2,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CustomJumbotron } from "@/components/ui/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { CustomPagination } from "@/components/ui/custom/CustomPagination"
 import { CustomBreadcrumbs } from "@/components/ui/custom/CustomBreadcrumbs"
-import { getHeroesByPage } from "@/heroes/actions/get-heroes-by-page.action"
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action"
+import { useQuery } from "@tanstack/react-query"
 
 export const HomePage = () => {
   const [activeTab, setActiveTab] = useState<
@@ -15,12 +16,22 @@ export const HomePage = () => {
     "villains"
   >('all');
 
-  useEffect(() => {
-    getHeroesByPage().then((heroes) => {
-      console.log({ heroes });
+  // useQuery pide 2 propiedades
+  // 1.- queryKey que es como el espacio en memoria que se desea guardar
+  // el resultado de la petición y se coloca como un arreglo.
+  // Dentro de ese arreglo se define como se quiere que luzca ese key
+  // 2. queryFn es la función que se desea disparar cuando eso suceda (es decir, la petición)
+  // 3. statleTime sirve para indcarle a tanstack cuánto tiempo considara el reultado de la petición como fresca.
 
-    })
-  }, [])
+  const { data } = useQuery({
+    queryKey: ['heroes'],
+    queryFn: () => getHeroesByPageAction(),
+    staleTime: 1000 * 60 * 5 // 5min
+  })
+
+  // useEffect(() => {
+  //   getHeroesByPage().then();
+  // }, [])
 
 
   return (
