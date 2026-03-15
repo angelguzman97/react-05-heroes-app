2a2d5c331606f0a1a7951d874db400af0874606f
Angel Guzman <angelteo0510@gmail.com>
2026-03-14 18:03:49 -0600

    Axios, variables de entorno y API

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -2,9 +2,10 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CustomJumbotron } from "@/components/ui/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { CustomPagination } from "@/components/ui/custom/CustomPagination"
 import { CustomBreadcrumbs } from "@/components/ui/custom/CustomBreadcrumbs"
+import { getHeroesByPage } from "@/heroes/actions/get-heroes-by-page.action"
 
 export const HomePage = () => {
   const [activeTab, setActiveTab] = useState<
@@ -13,6 +14,13 @@ export const HomePage = () => {
     "heroes" |
     "villains"
   >('all');
+
+  useEffect(() => {
+    getHeroesByPage().then((heroes) => {
+      console.log({ heroes });
+
+    })
+  }, [])
 
 
   return (
