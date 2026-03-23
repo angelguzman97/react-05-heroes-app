1e4fcb2c91a09390cdab6f958a4ac686400453c2
Angel Guzman <angelteo0510@gmail.com>
2026-03-23 17:01:30 -0600

    configuraciones para realizar test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    environment: 'jsdom',
+    globals: true
+  }
 })
