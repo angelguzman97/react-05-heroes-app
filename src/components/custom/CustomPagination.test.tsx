dd71eecfcc5a4f1bd439943a2fd5ff7e33587df4
Angel Guzman <angelteo0510@gmail.com>
2026-04-02 22:34:14 -0600

    Test - CustomPagination parte 2

--- src/components/custom/CustomPagination.test.tsx
+++ src/components/custom/CustomPagination.test.tsx
@@ -11,9 +11,9 @@ vi.mock('../ui/button', () => ({
     ),
 }));
 
-const renderWithRouter = (component: React.ReactElement) => {
+const renderWithRouter = (component: React.ReactElement, initialEntries?: string[]) => {
     return render(
-        <MemoryRouter>
+        <MemoryRouter initialEntries={initialEntries}>
             {component}
         </MemoryRouter>
     );
@@ -31,5 +31,28 @@ describe('CustomPagination', () => {
         expect(screen.getByText('3')).toBeDefined();
         expect(screen.getByText('4')).toBeDefined();
         expect(screen.getByText('5')).toBeDefined();
+    });
+
+    test('should disabled previous button when page is 1', () => {
+        renderWithRouter(<CustomPagination totalPages={4} />);
+        const previousButton = screen.getByText('Anteriores');
+        // screen.debug(previousButton);
+        expect(previousButton.getAttributeNames()).toContain('disabled');
+    });
+
+    test('should disabled next button when we are in the last page', () => {
+        renderWithRouter(<CustomPagination totalPages={5} />, ['/?page=5']);
+        const nextButton = screen.getByText('Siguientes');
+        // screen.debug(nextButton);
+        expect(nextButton.getAttributeNames()).toContain('disabled');
+    });
+    test('should disabled button 3 when we are in page 3', () => {
+        renderWithRouter(<CustomPagination totalPages={10} />, ['/?page=3']);
+        const button2 = screen.getByText('2');
+        const button3 = screen.getByText('3');
+        screen.debug(button2);
+        screen.debug(button3);
+        expect(button2.getAttribute('variant')).toBe('outline');
+        expect(button3.getAttribute('variant')).toBe('default');
     });
 });
